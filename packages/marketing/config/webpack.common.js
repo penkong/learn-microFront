0e2d2e74a5725ce8ee6ebcc9fe8a459758d11084
mazdak <nazemi.works@gmail.com>
2020-12-05 10:08:29 +0000

    init

---
+++ packages/marketing/config/webpack.common.js
@@ -0,0 +1,20 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            // react , es5 , 6 or ... maker
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+
+            // let use sth like async ...
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+};
---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,13 @@
+// mount function
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+
+const mount = (el) => ReactDOM.render(<App />, el);
+
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.getElementById("_marketing-dev-root");
+  if (devRoot) mount(devRoot);
+}
+
+export { mount };
